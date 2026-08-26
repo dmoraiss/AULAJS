5b91243d134864a65f5b8367cadc42a92b35087b
dmoraiss <luizfelipesoares453@gmail.com>
2026-08-26 17:31:14 -0300

    exercicios de JS , todas feitas em aula do senai

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ De 0°C a 15°C	❄️ Frio
 De 16°C a 25°C	😊 Agradável
 De 26°C a 35°C	🌡️ Quente
 Acima de 35°C	🔥 Muito quente
-*/
+
 
 
 const temperatura = Number ( prompt ( "Digite a temperatura atual !" ) )
@@ -185,6 +185,670 @@ if (Number.isNaN(temperatura)) {
     console.log("Muito Frio")
 
 }
+
+
+
+
+
+let peso = Number(prompt("Peso (Kg) "));
+let altura = Number(prompt("Altura (M) "));
+
+let imc = peso / (altura **2);
+
+console.log(imc.toFixed(2));
+alert(imc.toFixed(2));
+
+
+
+if (imc <=18 ) {
+    console.log("Baixo")
+} 
+else if ( imc > 18 && imc <= 24 ) {
+    console.log("normal")
+}
+else {
+    console.log("sobre preso")
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+//1.Verificar maioridade
+//Crie um programa que solicite a idade de uma pessoa e informe se ela é maior de idade (18 anos ou mais) ou menor de idade.
+
+    /*
+ let idade = Number(prompt ("Digite sua idade "))
+
+ if (Number.isNaN (idade)) {
+    console.log ("Digite apenas numeros ")
+ } else if ( idade >= 18) {
+    console.log ("Maior de Idade")
+ } else {
+    console.log ("Menor de Idade")
+ }
+    */
+
+//2.Positivo ou Negativo
+//Solicite um número ao usuário e informe se ele é positivo ou negativo. Considere o número 0 como negativo
+
+
+    /*
+let numero = Number(prompt ("Digite um numero "))
+
+ if (Number.isNaN (numero)) {
+    console.log ("Digite apenas numeros ")
+ } else if ( numero >= 1 ) {
+    console.log("Numero Possitivo");
+    alert("Numero Possitivo");
+ } else {
+    console.log("Numero Negativo");
+    alert("Numero Negativo");
+ }
+    */
+
+
+//3.Aprovação em uma prova
+//Peça ao usuário a nota final de um aluno (de 0 a 100). Caso a nota seja 60 ou maior, informe que ele foi Aprovado. Caso contrário, informe que foi Reprovado.
+
+/*
+let nota = Number(prompt ("Digite sua Nota "))
+
+ if (Number.isNaN (nota)) {
+    console.log ("Digite apenas numeros ")
+ } else if ( nota >= 60) {
+    console.log("Aluno Aprovado");
+    alert("Aluno Aprovado");
+ } else {
+    console.log("Aluno Reprovado");
+    alert("Aluno Reprovado");
+ }
+ */
+
+//4.Positivo, Negativo ou Zero
+//Leia um número informado pelo usuário e indique se ele é:
+//Positivo; 
+//Negativo; 
+//Zero. 
+
+    /*
+let numero = Number(prompt ("Digite um numero "))
+
+if (Number.isNaN (numero)) {
+    console.log ("Digite apenas numeros ")
+ } 
+ else if ( numero >= 1 ) {
+    console.log("Numero Possitivo");
+    alert("Numero Possitivo");
+ } 
+ else if (numero  <= -1 ) {
+    console.log("Numero Negativo");
+    alert("Numero Negativo");
+ } 
+ else if ( numero = 1 ) {
+    console.log("Numero 0");
+    alert("Numero 0");
+ }
+    */
+
+//5. Classificação por idade
+//Solicite a idade de uma pessoa e classifique-a conforme as regras abaixo:
+//Criança: de 0 a 12 anos; 
+//Adolescente: de 13 a 17 anos; 
+//Adulto: 18 anos ou mais. 
+//Caso seja informada uma idade negativa, exiba uma mensagem de idade inválida.
+
+    /*
+let idade = Number(prompt ("Digite sua idade "))
+
+ if (Number.isNaN (idade)) {
+
+    console.log ("Digite apenas numeros ")
+    alert("Digite apenas numeros");
+
+ } 
+    else if ( idade <= 12) {
+
+    console.log ("Criança")
+    alert("Criança");
+
+ } 
+    else if (idade <= 17) {
+
+    console.log ("Adolescente")
+    alert("Adolescente");
+
+ } 
+    else if (idade >= 18) {
+
+    console.log ("Adulto")
+    alert("Adulto");
+
+ } 
+    else if (idade <= -1) {
+
+     console.log ("idade inválida")
+    alert("idade inválida");
+    
+ }
+    */
+
+//6. Par ou Ímpar
+//Solicite um número inteiro ao usuário e informe se ele é par ou ímpar.
+
+/*
+let numero = Number(prompt ("Digite um numero "))
+
+if (Number.isNaN (numero)) {
+    console.log ("Digite apenas numeros ")
+ } 
+ else if ( numero % 2 === 0 ) {
+    console.log("Numero Par");
+    alert("Numero Par");
+ } 
+ else {
+    console.log("Numero Impar");
+    alert("Numero Impar");
+ } 
+*/
+
+/* ------------------------------------  */
+
+
+// Nível Intermediário
+
+
+/* ------------------------------------  */
+
+//1. Calculadora simples
+//Crie uma calculadora que:
+//Solicite o primeiro número; 
+//Solicite a operação desejada (+, -, * ou /); 
+//Solicite o segundo número. 
+//Ao final, exiba o resultado da operação escolhida. Caso seja informada uma operação inválida, exiba uma mensagem de erro.
+
+/*
+
+let numero1 = Number(prompt ("Digite o primerio numero "))
+let operador = (prompt ("Digite operador "))
+let numero2 = Number(prompt ("Digite o segundo numero "))
+
+
+let resultado;
+
+if (operador === "+") {
+
+    resultado = numero1 + numero2 ;
+    console.log(resultado);
+    alert(resultado);
+    
+} else if (operador === "-") {
+
+    resultado = numero1 - numero2 ;
+    console.log(resultado);
+    alert(resultado);
+
+} else if (operador === "*") {
+
+    resultado = numero1 * numero2 ;
+    console.log(resultado);
+    alert(resultado);
+
+} else if (operador === "/") {
+
+    if (numero2 == 0 ) {
+
+    console.log("Não é possivel realizar o resultado");
+    alert("Não  possivel realizar o resultado");
+
+    } else {
+
+    resultado = numero1 / numero2 ;
+    console.log(resultado);
+    alert(resultado);
+
+    }
+
+} else {
+    
+    console.log(Erro);
+}
+
+*/
+
+//2. Maior entre três números
+//Solicite três números diferentes ao usuário e informe qual deles é o maior.
+
+let numero1 = Number(prompt("Digite o primeiro numero"))
+let numero2 = Number(prompt("Digite o segundo numero"))
+let numero3 = Number(prompt("Digite o terceiro numero"))
+
+if (numero1 >= numero2 && numero1 >= numero3 ) {
+    console.log(" O numero " + numero1 + " e o maior ")
+
+} else if (numero2 >= numero1 && numero2 >= numero3 ) {
+    console.log(" O numero " + numero2 + " e o maior ")
+} else {
+    " O numero " + numero3 + " e o maior "
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
