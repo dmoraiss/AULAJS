cabc5f54feec5992b00fa4629bf8725ce8a93fc1
dmoraiss <luizfelipesoares453@gmail.com>
2026-09-03 17:52:38 -0300

    aula de function

---
+++ funcao.js
@@ -0,0 +1,49 @@
+
+// FUNCAO
+
+//Funcao é um conjunto de inscricoes que agrupam para realizar 
+
+
+function saudacoes() {
+    console.log("Ola, seja bem vindo ");
+    
+}
+
+saudacoes()
+saudacoes()
+saudacoes()
+
+
+function SolicitarNome() {
+    
+    let nomeInserido = prompt("Insira seu nome")
+alert("O nome inserido e " + nomeInserido)
+
+}
+
+
+SolicitarNome()
+SolicitarNome()
+SolicitarNome()
+SolicitarNome()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
